e5eee0e738c711bbe4ec5435d5052724d95264a0
Jorge Sardina <jorge@skilldevs.com>
2026-05-11 20:32:07 +0200

    migrate to spm

---
+++ macos/camera_macos/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "camera_macos",
+    platforms: [
+        .macOS("10.14")
+    ],
+    products: [
+        .library(name: "camera-macos", targets: ["camera_macos"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "camera_macos",
+            dependencies: [],
+            resources: []
+        )
+    ]
+)
